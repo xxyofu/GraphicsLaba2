b1ad809d59e13996066c539ae8b9369220add311
xxyofu <dayn12321dayn@gmail.com>
2025-10-19 17:51:55 +0300

    Second commit

--- laba.cpp
+++ laba.cpp
@@ -19,6 +19,7 @@ class Image1
 {
 public:
     int width, height;
+    std::string magic_number;
     std::vector<RGB> pixels;
 
     Image1(const string& filepath)
@@ -28,13 +29,27 @@ public:
             cout << "Cant open file" << endl;
             return;
         }
+        stream >> magic_number;
         stream.ignore(100, '\n');
         stream >> width >> height;
         stream.ignore(100, '\n');
         stream.ignore(100, '\n');
 
         pixels.resize(width * height);
-        stream.read(reinterpret_cast<char*>(pixels.data()), pixels.size() * sizeof(RGB));
+        if(magic_number=="P6"){
+            stream.read(reinterpret_cast<char*>(pixels.data()), pixels.size() * sizeof(RGB));
+        }else if (magic_number=="P3"){
+            for (int i = 0; i < width * height; i++) {
+                int r, g, b;
+                stream >> r >> g >> b;
+                pixels[i] = {static_cast<unsigned char>(r), 
+                            static_cast<unsigned char>(g), 
+                            static_cast<unsigned char>(b)};
+            }
+        }else{
+            return;
+        }
+        
     }
     void Replace_Pixel(int x, int y, unsigned char r, unsigned char g, unsigned char b)
     {
@@ -162,20 +177,37 @@ public:
     {
         for (int i = 1; i <= l; i += 1) 
         {
-            //cout << zero.x + i << ' '<< sin(2 * PI * ((double)i / step)) << '\n';
             DrawLine({zero.x+i,zero.y-int(log(double(i)/100.0))}, {zero.x+i+1,zero.y-int(log(double(i+1)/100.0))}, color);
         }
     }
-    bool Save_Canvas(const string& filepath)
+    bool Save_Canvas(const string& filepath, std::string magic_number)
     {
         ofstream stream(filepath, ios::binary);
         if (!stream) {
             cout << "Cant make file" << endl;
             return false;
         }
-        stream << "P6\n" << width << " " << height << "\n255\n";
-        stream.write(reinterpret_cast<const char*>(pixels.data()), pixels.size() * sizeof(RGB));
-        return true;
+        if(magic_number == "P6"){
+            stream << "P6\n" << width << " " << height << "\n255\n";
+            stream.write(reinterpret_cast<const char*>(pixels.data()), pixels.size() * sizeof(RGB));
+            return true;
+        }else if (magic_number=="P3"){
+            stream << "P3\n" << width << " " << height << "\n255\n";
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    const RGB& pixel = pixels[i * width + j];
+                    stream << static_cast<int>(pixel.r) << " "
+                        << static_cast<int>(pixel.g) << " "
+                        << static_cast<int>(pixel.b);
+                    if (j < width - 1) stream << " ";
+                    }
+                stream << "\n";
+            }
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 };
 
@@ -204,5 +236,5 @@ int main(int argc, char *argv[])
     canvas.DrawLine({ canvas.width / 2 + canvas.width / 20 ,canvas.height / 20, }, { canvas.width / 2, 0 }, color);
     canvas.DrawLine({ canvas.width / 2 - canvas.width / 20 ,canvas.height / 20, }, { canvas.width / 2, 0 }, color);
     canvas.DrawFunk({ canvas.width / 2, canvas.height / 2 }, canvas.width / 2 - 1,bl);
-    canvas.Save_Canvas(output);
+    canvas.Save_Canvas(output, "P3");
 }
